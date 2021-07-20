f6db12410416f1a7122bcf8bd72cdfa3be85287e
rick <rick@rickdeMacBook-Pro.local>
2021-07-20 17:52:29 +0800

    update

--- zeppelin-plugins/launcher/k8s-standard/src/test/java/org/apache/zeppelin/interpreter/launcher/K8sRemoteInterpreterProcessMinikubeTest.java
+++ zeppelin-plugins/launcher/k8s-standard/src/test/java/org/apache/zeppelin/interpreter/launcher/K8sRemoteInterpreterProcessMinikubeTest.java
@@ -1,27 +1,43 @@
 package org.apache.zeppelin.interpreter.launcher;
 
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodStatus;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
-import org.apache.zeppelin.interpreter.remote.RemoteInterpreterManagedProcess;
+import org.apache.zeppelin.interpreter.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.time.Instant;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 
 public class K8sRemoteInterpreterProcessMinikubeTest {
+    private static MiniZeppelin zeppelin;
+    private static InterpreterFactory interpreterFactory;
+    private static InterpreterSettingManager interpreterSettingManager;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        zeppelin = new MiniZeppelin();
+        zeppelin.start(K8sRemoteInterpreterProcessMinikubeTest.class);
+        interpreterFactory = zeppelin.getInterpreterFactory();
+        interpreterSettingManager = zeppelin.getInterpreterSettingManager();
+    }
+
+    @AfterClass
+    public static void tearDown() throws IOException {
+        if (zeppelin != null) {
+            zeppelin.stop();
+        }
+    }
 
     @Test
     public void testPredefinedPortNumbers() {
@@ -58,7 +74,82 @@ public class K8sRemoteInterpreterProcessMinikubeTest {
     }
 
     @Test
-    public void testK8sStartSuccessful() throws IOException {
+    public void testK8sStartSuccessful() throws IOException, InterpreterNotFoundException, InterpreterException {
+        // given
+        InterpreterSetting interpreterSetting = interpreterSettingManager.getInterpreterSettingByName("sh");
+        interpreterSetting.setProperty("zeppelin.k8s.interpreter.container.image", "local/zeppelin");
+        interpreterSetting.setProperty("ZEPPELIN_CONF_DIR", "/opt/zeppelin/conf");
+        interpreterSetting.setProperty("ZEPPELIN_HOME", "/opt/zeppelin");
+        interpreterSetting.setProperty("zeppelin.k8s.interpreter.container.imagePullPolicy", "Never");
+
+        // test shell interpreter
+        Interpreter interpreter = interpreterFactory.getInterpreter("sh", new ExecutionContext("anonymous", "note1", "test"));
+
+        InterpreterContext context = new InterpreterContext.Builder().setNoteId("note1").setParagraphId("paragraph_1").build();
+        InterpreterResult interpreterResult = interpreter.interpret("pwd", context);
+        assertEquals(interpreterResult.toString(), InterpreterResult.Code.SUCCESS, interpreterResult.code());
+    }
+
+    @Test
+    public void testK8sStartSparkSuccessful() throws IOException, InterpreterNotFoundException, InterpreterException {
+        // given
+        InterpreterSetting interpreterSetting = interpreterSettingManager.getInterpreterSettingByName("spark");
+        interpreterSetting.setProperty("zeppelin.k8s.interpreter.container.image", "local/zeppelin");
+        interpreterSetting.setProperty("ZEPPELIN_CONF_DIR", "/opt/zeppelin/conf");
+        interpreterSetting.setProperty("ZEPPELIN_HOME", "/opt/zeppelin");
+        interpreterSetting.setProperty("zeppelin.k8s.interpreter.container.imagePullPolicy", "Never");
+
+        interpreterSetting.setProperty("zeppelin.k8s.spark.container.imagePullPolicy", "Never");
+        interpreterSetting.setProperty("zeppelin.k8s.spark.container.image", "registry.cn-hangzhou.aliyuncs.com/streamcompute/spark-py:testv4");
+        interpreterSetting.setProperty("SPARK_HOME", "/spark");
+        interpreterSetting.setProperty("spark.master", "k8s://https://kubernetes.default.svc");
+        interpreterSetting.setProperty("zeppelin.spark.enableSupportedVersionCheck", "false");
+        //interpreterSetting.setProperty("spark.executor.memory", "512m");
+        interpreterSetting.setProperty("spark.kubernetes.container.image.pullPolicy", "Never");
+        interpreterSetting.setProperty("SPARK_PRINT_LAUNCH_COMMAND", "true");
+
+        interpreterSetting.setProperty("spark.user.name", "#{user}");
+        interpreterSetting.setProperty("zeppelin.spark.run.asLoginUser", "false");
+
+        interpreterSetting.setProperty("zeppelin.spark.useHiveContext", "false");
+        interpreterSetting.setProperty("zeppelin.pyspark.useIPython", "false");
+        interpreterSetting.setProperty("spark.driver.memory", "1g");
+
+        //interpreterSetting.setProperty("spark.executor.cores", "3");
+        interpreterSetting.setProperty("spark.executor.memory", "5g");
+        interpreterSetting.setProperty("spark.executor.memoryOverhead", "5g");
+        interpreterSetting.setProperty("zeppelin.spark.scala.color", "false");
+        interpreterSetting.setProperty("zeppelin.spark.deprecatedMsg.show", "false");
+
+        interpreterSetting.setProperty("spark.executor.instances", "1");
+
+        // test shell interpreter
+        Interpreter interpreter = interpreterFactory.getInterpreter("spark.spark", new ExecutionContext("user1", "note1", "test"));
+
+
+        InterpreterContext context = new InterpreterContext.Builder().setNoteId("note1").setParagraphId("paragraph_1").build();
+        /*
+        InterpreterResult interpreterResult = interpreter.interpret("sc.version", context);
+        assertEquals(interpreterResult.toString(), InterpreterResult.Code.SUCCESS, interpreterResult.code());
+
+         */
+
+
+        InterpreterResult interpreterResult = interpreter.interpret("sc.range(1,10).sum()", context);
+        assertEquals(interpreterResult.toString(), InterpreterResult.Code.SUCCESS, interpreterResult.code());
+        assertTrue(interpreterResult.toString(), interpreterResult.message().get(0).getData().contains("45"));
+
+    }
+
+
+
+
+
+
+
+/*
+    @Test
+    public void testK8sStartFailed() {
         // given
         KubernetesClient client = new DefaultKubernetesClient();
         Properties properties = new Properties();
@@ -75,65 +166,36 @@ public class K8sRemoteInterpreterProcessMinikubeTest {
                 file,
                 "local/zeppelin",
                 "shared_process",
-                "sh",
-                "shell",
+                "spark",
+                "myspark",
                 properties,
                 envs,
                 "zeppelin.server.service",
                 12320,
                 false,
                 "spark-container:1.0",
-                100000,
+                3000,
                 10,
                 false,
-                false);
+                true);
+
+
 
         ExecutorService service = Executors.newFixedThreadPool(1);
         service.submit(new PodStatusChecker(client, intp.getNamespace(), intp.getPodName(), intp));
 
-        intp.start("TestUser");
-        // then
-        assertEquals("Running", intp.getPodPhase());
-    }
-
-    class PodStatusChecker implements Runnable {
-
-        private final KubernetesClient client;
-        private final String namespace;
-        private final String podName;
-        private final RemoteInterpreterManagedProcess process;
-
-        public PodStatusChecker(
-                KubernetesClient client,
-                String namespace,
-                String podName,
-                RemoteInterpreterManagedProcess process) {
-            this.client = client;
-            this.namespace = namespace;
-            this.podName = podName;
-            this.process = process;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Instant timeoutTime = Instant.now().plusSeconds(10);
-                while (timeoutTime.isAfter(Instant.now())) {
-                    Pod pod = client.pods().inNamespace(namespace).withName(podName).get();
-                    if (pod != null) {
-                        TimeUnit.SECONDS.sleep(1);
-                        String phase = pod.getStatus().getPhase();
-                        System.out.println(phase);
-                        if (phase.equals("Running")) {
-                            process.processStarted(12320, "testing");
-                        }
-                    } else {
-                        TimeUnit.MILLISECONDS.sleep(100);
-                    }
-                }
-            } catch (InterruptedException e) {
-                // Do nothing
-            }
+        // should throw an IOException
+        try {
+            intp.start("TestUser");
+            fail("We excepting an IOException");
+        } catch (IOException e) {
+            assertNotNull(e);
+            // Check that the Pod is deleted
+            assertNull(
+                    server.getClient().pods().inNamespace(intp.getNamespace()).withName(intp.getPodName())
+                            .get());
         }
     }
+*/
+
 }
---
+++ zeppelin-plugins/launcher/k8s-standard/src/test/java/org/apache/zeppelin/interpreter/launcher/MiniZeppelin.java
@@ -0,0 +1,76 @@
+package org.apache.zeppelin.interpreter.launcher;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.zeppelin.conf.ZeppelinConfiguration;
+import org.apache.zeppelin.display.AngularObjectRegistryListener;
+import org.apache.zeppelin.helium.ApplicationEventListener;
+import org.apache.zeppelin.interpreter.InterpreterFactory;
+import org.apache.zeppelin.interpreter.InterpreterSettingManager;
+import org.apache.zeppelin.interpreter.remote.RemoteInterpreterProcessListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+public class MiniZeppelin {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(MiniZeppelin.class);
+
+    protected InterpreterSettingManager interpreterSettingManager;
+    protected InterpreterFactory interpreterFactory;
+    protected File zeppelinHome;
+    private File confDir;
+    private File notebookDir;
+    protected ZeppelinConfiguration conf;
+
+    public void start(Class clazz) throws IOException {
+        zeppelinHome = new File("../../..");
+        System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_HOME.getVarName(),
+                zeppelinHome.getAbsolutePath());
+        confDir = new File(zeppelinHome, "conf_" + clazz.getSimpleName());
+        notebookDir = new File(zeppelinHome, "notebook_" + clazz.getSimpleName());
+        confDir.mkdirs();
+        notebookDir.mkdirs();
+        LOGGER.info("ZEPPELIN_HOME: " + zeppelinHome.getAbsolutePath());
+        FileUtils.copyFile(new File(zeppelinHome, "conf/log4j.properties"), new File(confDir, "log4j.properties"));
+        FileUtils.copyFile(new File(zeppelinHome, "conf/log4j2.properties"), new File(confDir, "log4j2.properties"));
+        FileUtils.copyFile(new File(zeppelinHome, "conf/log4j_yarn_cluster.properties"), new File(confDir, "log4j_yarn_cluster.properties"));
+        System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_CONF_DIR.getVarName(), confDir.getAbsolutePath());
+        System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_NOTEBOOK_DIR.getVarName(), notebookDir.getAbsolutePath());
+        System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_INTERPRETER_CONNECT_TIMEOUT.getVarName(), "120000");
+        conf = ZeppelinConfiguration.create();
+        conf.setRunMode(ZeppelinConfiguration.RUN_MODE.K8S);
+        conf.setProperty("zeppelin.k8s.portforward", "true");
+        conf.setProperty("zeppelin.k8s.spark.container.image", "registry.cn-hangzhou.aliyuncs.com/streamcompute/spark-py:testv4");
+        conf.setProperty("zeppelin.interpreter.connect.timeout", "600000");
+
+        interpreterSettingManager = new InterpreterSettingManager(conf,
+                mock(AngularObjectRegistryListener.class), mock(RemoteInterpreterProcessListener.class), mock(ApplicationEventListener.class));
+        interpreterFactory = new InterpreterFactory(interpreterSettingManager);
+    }
+
+    public void stop() throws IOException {
+        interpreterSettingManager.close();
+        FileUtils.deleteDirectory(confDir);
+        FileUtils.deleteDirectory(notebookDir);
+    }
+
+    public File getZeppelinHome() {
+        return zeppelinHome;
+    }
+
+    public File getZeppelinConfDir() {
+        return confDir;
+    }
+
+    public InterpreterFactory getInterpreterFactory() {
+        return interpreterFactory;
+    }
+
+    public InterpreterSettingManager getInterpreterSettingManager() {
+        return interpreterSettingManager;
+    }
+}
